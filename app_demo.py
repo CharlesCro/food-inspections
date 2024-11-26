5ee0304ac6ded63dc8535c449ae057bec131a7e4
CharlesCro <charlescrocicchia@gmail.com>
2024-11-26 16:35:58 +0100

    Modelling Begun, EDA expanded
    playing a game of  cat and mouse with .gitignore making sure no extra files are shown.

--- app_demo.py
+++ app_demo.py
@@ -1,17 +1,16 @@
 # Imports
+import string
 import streamlit as st
 import pandas as pd
-import altair as alt
 import seaborn as sns
 import matplotlib.pyplot as plt
-import string
 from code.plotting.st_plots import st_plots
 
 # Style Customization
 plt.rcParams['font.family'] = 'DejaVu Sans'
 
 # load database
-df = pd.read_csv('./data/processed_chicago.csv')
+df = pd.read_csv('data/processed_chicago.csv')
 
 def remove_punctuation(text):
     '''
@@ -29,26 +28,6 @@ st.sidebar.info(
     """
     This app allows users to search for restaurants, select a location, 
     and view graphs based on food safety data related to the selected restaurant.
-
-    TO DO:
-    - success rate graph (finally a nice pie chart)
-
-    - re-inspection stats (bar chart comparing first to second visit, and another violations list, also need to show if they passed after reinspection or not)
-
-    - Summary Graphs of Other Locations of that restaurant to compare ("Maybe go to this McDonald's instead!") sort of idea.
-
-    - Find way for common disliked violations to pop out
-    examples (rodents, cross contamination, dirty bathrooms, dirty food handling, emphasis on things which a customer would want to know without getting too gross)
-
-    - Draft app description/'help' page for how to use and interpret app
-
-    - a hue for the text depending on risk level of restaurant
-
-    - show inspection types in x ticks of inspection history graph
-
-    - a specific graph for how the restaurant LAST performed.
-
-    - Have inspection type in the xtick underneath date?
     """
 )
 
@@ -97,35 +76,8 @@ if selected_location:
                 ''')
     st_plots.plot_inspection_history(selected_data)
     
-
-
 st.markdown("---")
 
-
-
-
-# # Search bar
-# restaurant_name = st.text_input('Search for a restaurant:', '')
-
-# # Filter DataFrame by restaurant name (case-insensitive)
-# filtered_by_name = chicago[chicago['aka_name'].str.contains(restaurant_name, case=False, na=False)]
-
-# if not filtered_by_name.empty:
-#     # Dropdown to pick a location
-#     location = st.selectbox("Choose a location:", filtered_by_name["address"].unique())
-
-#     # Filter DataFrame by location and license number
-#     single_restaurant = filtered_by_name[filtered_by_name["address"] == location]
-
-#     # Display the plot
-#     if not single_restaurant.empty:
-#         st.header('Altair Plot Test')
-#         st_plots.test_plot_altair(single_restaurant)
-#         st.header('Seaborn Plot Test')
-#         st_plots.plot_inspection_history(single_restaurant)
-
-# else:
-#     st.write("No matching restaurant found.")
 
 
 
